5d5fce797d99ec2c61e939d652610f34445fe721
Chris Horuk <chris.horuk@appfolio.com>
2015-07-23 17:05:50 -0700

    ch - skip csrf check for slack action, render some basic json response

--- app/controllers/slack_coordinator_controller.rb
+++ app/controllers/slack_coordinator_controller.rb
@@ -1,7 +1,10 @@
 class SlackCoordinatorController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :receive
   # This will be the action that gets hit by slack when
   # someone uses the trigger word in the right channel.
   def receive
     puts params
+
+    render json: { text: 'Yea I heard you' }
   end
 end
