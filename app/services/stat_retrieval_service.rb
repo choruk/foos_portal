1a4b4d5096152ec92e35dc0b2ccb393be96ee679
Chris Horuk <chris.horuk@appfolio.com>
2015-07-24 12:40:13 -0700

    sort by win ratio then by slack user name

--- app/services/stat_retrieval_service.rb
+++ app/services/stat_retrieval_service.rb
@@ -5,13 +5,13 @@ class StatRetrievalService
       win_ratio: win_ratio
     }
 
-    sorted_win_ratios = [[win_ratio, user.id]]
+    sorted_win_ratios = [[win_ratio, user.slack_user_name]]
     User.where('id != ?', user.id).each do |other_user|
       user_win_ratio = other_user.games_won.to_f / other_user.games_finished.to_f
-      sorted_win_ratios.push [user_win_ratio, other_user.id]
+      sorted_win_ratios.push [user_win_ratio, other_user.slack_user_name]
     end
-    sorted_win_ratios.sort!
-    user_stats[:rank] = sorted_win_ratios.index([win_ratio, user.id])+1
+    sorted_win_ratios.sort! { |x,y| x.first <=> y.first && x.last <=> y.last }
+    user_stats[:rank] = sorted_win_ratios.index([win_ratio, user.slack_user_name])+1
 
     user_stats
   end
