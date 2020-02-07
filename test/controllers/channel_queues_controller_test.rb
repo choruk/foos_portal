4640eee4f1b5b09b9004295d53ca92af10466988
John Mangel <john.mangel@appfolio.com>
2020-02-07 15:04:29 -0800

    jm - remove unnecessary variable initialization in ChannelQueuesControllerTest

--- test/controllers/channel_queues_controller_test.rb
+++ test/controllers/channel_queues_controller_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 
 class ChannelQueuesControllerTest < ActionController::TestCase
   def test_index
-    channel_queue = ChannelQueue.create!(slack_channel_name: 'my-channel', slack_channel_id: 'C123')
-
-    first_user = User.create!(slack_user_name: 'first.user', slack_user_id: 'U123')
-    ChannelQueueMembership.create!(user: first_user, channel_queue: channel_queue)
-
-    second_user = User.create!(slack_user_name: 'second.user', slack_user_id: 'U124')
-    ChannelQueueMembership.create!(user: second_user, channel_queue: channel_queue)
-
     expected_response_body = { text: 'response body text' }
     ChannelQueues::ResponseRetriever.expects(:retrieve).with('list', 'C123', 'my-channel', 'U123', 'my.user').returns(expected_response_body)
 
