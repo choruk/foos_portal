9454aebbdfc6fb17f96aae2c91ca7684af85760f
John Mangel <john.mangel@appfolio.com>
2020-02-21 13:07:49 -0800

    jm - fix broken ChannelQueueMembershipTest cases

--- test/models/channel_queue_membership_test.rb
+++ test/models/channel_queue_membership_test.rb
@@ -5,12 +5,12 @@ class ChannelQueueMembershipTest < ActiveSupport::TestCase
     channel_queue_membership = ChannelQueueMembership.new(user: User.create!(slack_user_id: '1', slack_user_name: 'joe', rank: 1500))
     assert_not_predicate channel_queue_membership, :valid?
 
-    channel_queue_membership.channel_queue = ChannelQueue.create!(channel: 'ev-chargers-of-appfolio')
+    channel_queue_membership.channel_queue = ChannelQueue.create!(slack_channel_name: 'ev-chargers-of-appfolio', slack_channel_id: 'U1234')
     assert_predicate channel_queue_membership, :valid?
   end
 
   def test_validates_presence_of_user
-    channel_queue_membership = ChannelQueueMembership.new(channel_queue: ChannelQueue.create!(channel: 'ev-chargers-of-appfolio'))
+    channel_queue_membership = ChannelQueueMembership.new(channel_queue: ChannelQueue.create!(slack_channel_name: 'ev-chargers-of-appfolio', slack_channel_id: 'U1234'))
     assert_not_predicate channel_queue_membership, :valid?
 
     channel_queue_membership.user = User.create!(slack_user_id: '1', slack_user_name: 'joe', rank: 1500)
@@ -18,7 +18,7 @@ class ChannelQueueMembershipTest < ActiveSupport::TestCase
   end
 
   def test_validates_uniqueness_of_user_and_channel
-    channel_queue = ChannelQueue.create!(channel: 'ev-chargers-of-appfolio')
+    channel_queue = ChannelQueue.create!(slack_channel_name: 'ev-chargers-of-appfolio', slack_channel_id: 'U1234')
     user = User.create!(slack_user_id: '1', slack_user_name: 'joe', rank: 1500)
 
     ChannelQueueMembership.create!(channel_queue: channel_queue, user: user)
@@ -29,12 +29,12 @@ class ChannelQueueMembershipTest < ActiveSupport::TestCase
     different_user_queue_membership = ChannelQueueMembership.new(channel_queue: channel_queue, user: User.create!(slack_user_id: '2', slack_user_name: 'different', rank: 1500))
     assert_predicate different_user_queue_membership, :valid?
 
-    different_channel_queue_membership = ChannelQueueMembership.new(channel_queue: ChannelQueue.create!(channel: 'different'), user: user)
+    different_channel_queue_membership = ChannelQueueMembership.new(channel_queue: ChannelQueue.create!(slack_channel_name: 'different', slack_channel_id: 'U1235'), user: user)
     assert_predicate different_channel_queue_membership, :valid?
   end
 
   def test_belongs_to_channel_queue
-    channel_queue = ChannelQueue.create!(channel: 'ev-chargers-of-appfolio')
+    channel_queue = ChannelQueue.create!(slack_channel_name: 'ev-chargers-of-appfolio', slack_channel_id: 'U1234')
     user = User.create!(slack_user_id: '1', slack_user_name: 'joe', rank: 1500)
 
     channel_queue_membership = ChannelQueueMembership.create!(channel_queue: channel_queue, user: user)
@@ -43,7 +43,7 @@ class ChannelQueueMembershipTest < ActiveSupport::TestCase
   end
 
   def test_belongs_to_user
-    channel_queue = ChannelQueue.create!(channel: 'ev-chargers-of-appfolio')
+    channel_queue = ChannelQueue.create!(slack_channel_name: 'ev-chargers-of-appfolio', slack_channel_id: 'U1234')
     user = User.create!(slack_user_id: '1', slack_user_name: 'joe', rank: 1500)
 
     channel_queue_membership = ChannelQueueMembership.create!(channel_queue: channel_queue, user: user)
