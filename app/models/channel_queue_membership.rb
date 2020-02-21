b9997b84e6297197165667e282b34335e938ef7f
John Mangel <john.mangel@appfolio.com>
2020-02-21 13:34:11 -0800

    jm - notify users if joining closed queue

--- app/models/channel_queue_membership.rb
+++ app/models/channel_queue_membership.rb
@@ -5,5 +5,18 @@ class ChannelQueueMembership < ActiveRecord::Base
   validates_presence_of :user_id, :channel_queue_id
   validates :user_id, uniqueness: { scope: :channel_queue_id }
 
-  scope :active, -> { where('channel_queue_memberships.created_at >= ?', Time.now.in_time_zone('Pacific Time (US & Canada)').beginning_of_day + 7.hours)}
+  scope :active, -> { where('channel_queue_memberships.created_at >= ?', ChannelQueueMembership.queue_open_time)}
+  scope :inactive, -> { where('channel_queue_memberships.created_at < ?', ChannelQueueMembership.queue_open_time)}
+
+  def active?
+    created_at >= ChannelQueueMembership.queue_open_time
+  end
+
+  def inactive?
+    !active?
+  end
+
+  def self.queue_open_time
+    Time.now.in_time_zone('Pacific Time (US & Canada)').beginning_of_day + 7.hours
+  end
 end
