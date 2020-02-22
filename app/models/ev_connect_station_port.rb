e7fba4e11a65466d3c8207bb4d4c802bb09abab3
Steven Wang <32946195+swangs@users.noreply.github.com>
2020-02-21 16:21:19 -0800

    Revert "Intoduce Evacancy bot"

--- app/models/ev_connect_station_port.rb
+++
@@ -1,8 +0,0 @@
-class EvConnectStationPort < ActiveRecord::Base
-  validates_presence_of :qr_code, :port_status
-  validates_uniqueness_of :qr_code
-
-  def status_changed?(new_status)
-    port_status != new_status
-  end
-end
